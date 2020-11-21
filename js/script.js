cb06b9907a630503854284df4f5f91ddb3d8d979
Максим <37771198+MasterJ0y@users.noreply.github.com>
2020-11-21 18:27:06 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.onload = function() {
 	let header = document.getElementById('header');
 
+	if (window.scrollY > 25) header.classList.add("header-scrolled");
+
 	window.addEventListener('scroll', function() {
 		if (window.scrollY > 25) {
 			header.classList.add("header-scrolled")
