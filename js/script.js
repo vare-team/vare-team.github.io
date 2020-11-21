1b2bfc12e22960463fad9f018a39545e19c1dbe5
Максим <37771198+MasterJ0y@users.noreply.github.com>
2020-11-21 21:20:37 +0300

    Mobile menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 window.onload = function() {
-	let header = document.getElementById('header');
+	let header = document.getElementById('header'),
+			headernav = document.getElementById('header-nav');
+
+	document.getElementById('mobile-menu').addEventListener("click", toggleHamburger);
+
+	function toggleHamburger(){
+		headernav.classList.toggle("showNav");
+		header.classList.toggle("header-background");
+	}
 
 	if (window.scrollY > 25) header.classList.add("header-scrolled");
 
