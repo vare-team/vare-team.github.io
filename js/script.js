14f2b415494923f37297d4cd6d59f3303b29e7ac
Максим <37771198+MasterJ0y@users.noreply.github.com>
2020-11-22 12:42:24 +0300

    Click outside

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 window.onload = function() {
 	let header = document.getElementById('header'),
-			headernav = document.getElementById('header-nav');
+			headernav = document.getElementById('header-nav'),
+			mobilebutton = document.getElementById('mobile-menu');
 
-	document.getElementById('mobile-menu').addEventListener("click", toggleHamburger);
+
+	window.addEventListener('click', function({target}){
+		if (mobilebutton.contains(target)){
+			toggleHamburger();
+		} else {
+			removeHamburger();
+		}
+	});
+
 
 	function toggleHamburger(){
 		headernav.classList.toggle("showNav");
 		header.classList.toggle("header-background");
 	}
+	function removeHamburger(){
+		headernav.classList.remove("showNav");
+		header.classList.remove("header-background");
+	}
+
 
 	if (window.scrollY > 25) header.classList.add("header-scrolled");
 
